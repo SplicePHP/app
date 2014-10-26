118fef94db21dc1b45066f8165fb9931a5aebd68
Dieter Gribnitz <admin@splicephp.com>
2014-10-26 10:18:56 +0200

    no message

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+var lib = 'webroot/lib/';
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-git');
+    grunt.loadNpmTasks('grunt-composer');
+    grunt.loadNpmTasks('grunt-hub');
+    grunt.loadNpmTasks('grunt-dev-update');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower: {
+            install: {
+                //just run 'grunt bower:install'
+                options: {
+                    targetDir: './webroot/lib',
+                    layout: 'byComponent',
+                    install: true,
+                    update: true,
+                    verbose: true,
+                    cleanTargetDir: false,
+                    cleanBowerDir: false,
+                    bowerOptions: {}
+                }
+            }
+        },
+        composer: {
+            options: {
+                usePhp: true,
+                composerLocation: 'composer.phar'
+            }
+        },
+		shell: {
+            composer: {
+                options: {stdout: true, stderr: true},
+                command: [
+                    'test -f composer.phar || curl -sS https://getcomposer.org/installer | php -d allow_url_fopen=On',
+                    'php -d allow_url_fopen=On composer.phar self-update',
+                    'php -d allow_url_fopen=On composer.phar update -v -n'
+                ].join('&&')
+            },
+            compile: {
+                options: {stdout: true, stderr: true},
+                command: [
+                    'src/Console/cake build --all true --prefix admin -f 1'
+                ].join('&&')
+            },
+            "update-all": {
+              options: {stdout: true, stderr: true},
+              command: [
+                'src/Console/cake automation update'
+              ].join('&&')
+            }
+		},
+		hub: {
+		    all: {
+		      src: ['plugins/*/Gruntfile.js'],
+		      tasks: ['update']
+		    },
+            watch: {
+                src: ['plugins/*/Gruntfile.js'],
+                tasks: ['watch']
+            }
+		},
+		watch: {
+            scripts: {
+              files: ['config/compile.txt'],
+              tasks: ['shell:compile'],
+              options: {
+                spawn: false
+              }
+            }
+		}
+    });
+
+	grunt.registerTask('watch-all', [
+		'hub:watch',
+		'watch'
+	]);
+
+  grunt.registerTask('update', [
+    'shell:update-all'
+  ]);
+};
